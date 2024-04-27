e310c57f04a4be5b168bcf314ce6a037d18e7719
bhuckins <52389660+bhuckins@users.noreply.github.com>
2024-04-26 22:35:00 -0700

    New .NET Core 8.0 PlanetFinder

---
+++ PlanetFinder/Pages/Echo/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlanetFinder.Pages.Echo
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
